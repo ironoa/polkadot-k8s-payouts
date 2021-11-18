5398ae39e4b6eb2be228b0440c3870df8d7dde28
Nexus2k <philipp@nexus-informatik.ch>
2021-11-18 15:43:16 +0100

    Migrate apiversion & add CI integration

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+
+run_tests() {
+    echo Running tests...
+}
+
+teardown() {
+    helmfile delete --purge
+}
+
+main(){
+    if [ -z "$KEEP_W3F_BASE_SERVICES" ]; then
+        trap teardown EXIT
+    fi
+
+    source /scripts/build-helmfile.sh
+
+    run_tests
+}
+
+main
--- scripts/uninstallLocal.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-CLUSTER_NAME="monitoring"
-
-kind delete cluster --name $CLUSTER_NAME
